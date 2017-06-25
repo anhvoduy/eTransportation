1c69d2e3849a04e536c0f0ddb73d6d6af378a4cd
duyanh <duyanh2005@gmail.com>
2017-06-25 11:30:48 +0700

    update

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
 ﻿const http = require('http');
-// const server = require('./server');
-// http.createServer(server).listen(server.get('port'), function () {
-//     console.log('eInventory Web & API is running on port:' + server.get('port'));    
-// });
+const server = require('./server');
 
-const server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
+http.createServer(server).listen(server.get('port'), function () {
+    console.log('eInventory Web & API is running on port:' + server.get('port'));    
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-console.log("Server running at http://localhost:%d", port);
+// const server = http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello Azure!");
+// });
+
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+// console.log("Server running at http://localhost:%d", port);
