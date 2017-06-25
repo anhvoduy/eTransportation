4e8e4f4c331515f142c3ed50dabb9a9bba26ed92
duyanh <duyanh2005@gmail.com>
2017-06-25 11:17:36 +0700

    clean up

--- server/app.js
+++ server/app.js
@@ -1,11 +1,4 @@
-﻿// var http = require('http');
-
-// http.createServer(server).listen(server.get('port'), function () {
-//     console.log('eInventory Web & API is running on port:' + server.get('port'));    
-// });
-
-
-var http = require('http');
+﻿var http = require('http');
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Azure!");
