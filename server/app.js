8ecf5fbe8eba29ce6048e2beb2361398ad1d2cc0
duyanh <duyanh2005@gmail.com>
2017-06-25 11:11:34 +0700

    init sites

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
-﻿var http = require('http');
+﻿const http = require('http');
+const server = require('./server');
 
 http.createServer(server).listen(server.get('port'), function () {
     console.log('eInventory Web & API is running on port:' + server.get('port'));    
