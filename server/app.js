206b89e09b3915983745df1144b5bc00a3d557c9
duyanh <duyanh2005@gmail.com>
2017-06-25 11:28:34 +0700

    update

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
-﻿var http = require('http');
-var server = http.createServer(function(request, response) {
+﻿const http = require('http');
+// const server = require('./server');
+
+const server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Azure!");
 });
 
-var port = process.env.PORT || 1337;
+const port = process.env.PORT || 1337;
 server.listen(port);
 console.log("Server running at http://localhost:%d", port);
