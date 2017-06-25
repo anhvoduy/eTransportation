d64cdea0551b3d035a28d2201a0633cc2717a784
duyanh <duyanh2005@gmail.com>
2017-06-25 11:16:03 +0700

    deploy azure

--- server/app.js
+++ server/app.js
@@ -1,5 +1,16 @@
-﻿var http = require('http');
+﻿// var http = require('http');
 
-http.createServer(server).listen(server.get('port'), function () {
-    console.log('eInventory Web & API is running on port:' + server.get('port'));    
+// http.createServer(server).listen(server.get('port'), function () {
+//     console.log('eInventory Web & API is running on port:' + server.get('port'));    
+// });
+
+
+var http = require('http');
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Azure!");
 });
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+console.log("Server running at http://localhost:%d", port);
