2590ce0f1fa5cf1b0e60a02d624e71df972afe5e
duyanh <duyanh2005@gmail.com>
2017-06-25 14:14:56 +0700

    use express (quick fixed)

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-﻿// const http = require('http');
-// const server = require('./server');
+﻿const http = require('http');
+const server = require('./server');
 
-// http.createServer(server).listen(server.get('port'), function () {
-//     console.log('eInventory Web & API is running on port:' + server.get('port'));    
-// });
-
-const http = require('http');
-const server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
+http.createServer(server).listen(server.get('port'), function () {
+    console.log('eInventory Web & API is running on port:' + server.get('port'));    
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-console.log("Server running at http://localhost:%d", port);
+// const http = require('http');
+// const server = http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello Azure!");
+// });
+
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+// console.log("Server running at http://localhost:%d", port);
