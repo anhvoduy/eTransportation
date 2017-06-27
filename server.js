31f9b1fd393ba2a62ba70d0334a9e7a34d4450b8
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-06-27 17:41:52 +0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const server = express();
 //server.set('port', process.env.PORT || 3000);
 server.set('port', 3000);
 
-server.use('/', express.static(path.join(__dirname, 'publish'), { index: 'default.html' }));
 server.use('/admin', express.static(path.join(__dirname, 'admin'), { index: 'default.html' }));
+server.use('/publish', express.static(path.join(__dirname, 'publish'), { index: 'default.html' }));
+server.use('/transport', express.static(path.join(__dirname, 'transport'), { index: 'default.html' }));
+
+server.use('/', express.static(path.join(__dirname, 'transport'), { index: 'default.html' }));
 
 module.exports = server;
