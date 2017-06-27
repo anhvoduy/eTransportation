e05f1153626578e1c9e2140b824712016ad3cf01
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-06-27 17:39:04 +0700

    add transport template

---
+++ routes/api.js
@@ -0,0 +1,65 @@
+﻿// Dependencies
+var express = require('express');
+var router = express.Router();
+
+// sample Authentication
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// our configuration
+var auth = require('../config/auth');
+var config = require('../config/config');
+var constant = require('../config/constant');
+var dbContext = require('../config/dbContext');
+var errorHelper = require('../config/errorHelper');
+var userService = require('../services/userService');
+
+
+// routers: use to test
+router.get('/', function (req, res, next) {
+    res.json({ message: 'eTransport method GET() is success' });
+    console.log('%s %s — %s', (new Date).toString(), req.method, req.url);	
+    next();
+});
+
+router.post('/', function (req, res, next) {
+    res.json({ message: 'eTransport method POST() is success' });
+    console.log('%s %s — %s', (new Date).toString(), req.method, req.url);
+    next();
+});
+
+// routers: use to authenticate
+router.post('/authenticate', function (req, res, next) {
+	console.log('authenticate ...');
+	return next();
+});
+
+router.post('/login', function (req, res, next) {
+	passport.authenticate('local', function (err, result) {
+		if (err) { return next(err); }
+		if (!result.success) {
+			console.log('Login is failed ...');
+			res.status(404).json({
+				success: false,
+				message: { code: 'ERROR_UNAUTHENTICATION', message: 'Username and Password is invalid.' }
+			});
+		} else {
+			console.log('Login is success ...');
+			var token = jwt.sign(result.user, config.secretKey, { expiresIn: 60 * 60 * 24 * 1 });
+			//console.log(token);
+			res.status(200).json({
+				success: true,
+				message: { code: 'SUCCESS_AUTHENTICATION', message: 'Login is successful.' },                
+				user: { username: result.user.username, token: token },
+			});
+		}
+	})(req, res, next);
+});
+
+router.get('/logout', function (req, res) {
+	console.log('Log out current user...');
+});
+
+// return Router
+module.exports = router;
