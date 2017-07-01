f21d3d5c139b27eef1baf7bd93949aed7e2777e4
duyanh <duyanh2005@gmail.com>
2017-07-01 23:31:27 +0700

    add pages

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ server.use('/admin', express.static(path.join(__dirname, 'admin'), { index: 'def
 server.use('/publish', express.static(path.join(__dirname, 'publish'), { index: 'default.html' }));
 server.use('/transport', express.static(path.join(__dirname, 'transport'), { index: 'default.html' }));
 
-server.use('/', express.static(path.join(__dirname, 'transport'), { index: 'default.html' }));
+const webpath = path.join(__dirname, 'transport'); 
+server.use('/', express.static(webpath, { index: 'default.html' }));
+server.use('/about-us', express.static(webpath , { index: 'about-us.html' }));
+server.use('/contact-us', express.static(webpath, { index: 'contact-us.html' }));
+server.use('/login', express.static(webpath, { index: 'login.html' }));
 
 module.exports = server;
