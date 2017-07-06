c27476993b7567122c17c0c5cf6a1e55ce146422
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-06 15:03:12 +0700

    - page service-us.html - our-service.mp4 - menu link

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const webpath = path.join(__dirname, 'transport');
 server.use('/', express.static(webpath, { index: 'default.html' }));
 server.use('/about-us', express.static(webpath , { index: 'about-us.html' }));
 server.use('/contact-us', express.static(webpath, { index: 'contact-us.html' }));
+server.use('/service-us', express.static(webpath, { index: 'service-us.html' }));
 server.use('/login', express.static(webpath, { index: 'login.html' }));
 
 module.exports = server;
