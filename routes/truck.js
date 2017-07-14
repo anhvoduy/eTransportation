e4349345ec70f7240ce52404df1cd980f0c91314
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-14 11:49:53 +0700

    add trucks management

---
+++ routes/truck.js
@@ -0,0 +1,37 @@
+﻿// Dependencies
+var express = require('express');
+var router = express.Router();
+var Q = require('q');
+var auth = require('../services/authService');
+var truckService = require('../services/truckService');
+
+// Routers
+router.get('/items', function (req, res, next) {
+	var trucks = truckService.getTrucks();
+	res.status(200).json(trucks);
+	next();
+});
+
+router.get('/items/:id', function (req, res, next) {
+    var truck = truckService.getTruckById(100);
+	res.status(200).json(truck);
+	next();
+});
+
+router.post('/create', function (req, res, next) {
+	// create truck;
+	next();
+});
+
+router.put('/update', function (req, res, next) {
+	// edit truck;
+	next();
+});
+
+router.delete('/delete', function (req, res, next) {
+	// edit truck;
+	next();
+});
+
+// return Router
+module.exports = router;
---
+++ services/truckService.js
@@ -0,0 +1,30 @@
+﻿const _ = require('lodash');
+const Q = require('q');
+const data = require('../database/sampleData');
+
+// Constructor
+const Factory = function () { 
+}
+
+Factory.prototype.getTrucks = function(){
+    return data.myProfile();
+}
+
+Factory.prototype.getTruckById = function(){
+    return data.getUser();
+}
+
+Factory.prototype.createTruck = function(){
+    return data.getUsers();
+}
+
+Factory.prototype.updateTruck = function(){
+    return data.getUsers();
+}
+
+Factory.prototype.deleteTruck = function(){
+    return data.getMenus();
+}
+
+// Export
+module.exports = new Factory;
