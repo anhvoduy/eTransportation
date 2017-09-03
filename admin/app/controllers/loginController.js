dcd05d478256793f4925580a7f79cab0363fca1c
duyanh <duyanh2005@gmail.com>
2017-09-03 20:26:25 +0700

    clean up

--- admin/app/controllers/loginController.js
+++ admin/app/controllers/loginController.js
@@ -3,28 +3,9 @@
 	app.controller('loginController', loginController);
 	loginController.$inject = ['$rootScope', '$scope', '$location', 'authService'];
 	function loginController($rootScope, $scope, $location, authService) {		
-		function activate() {
-			console.log(' init loginController ..... ');			
+		function activate() {			
 			authService.clearCredentials(); // reset login status
 		}
-
-		// var animateLogin = function(){
-        //     $('formLogin').animate({height: "toggle", opacity: "toggle"}, "slow");
-		// }
-		
-		// $scope.createAccount = function(){
-        //     console.log('---createAccount---');
-        //     animateLogin();
-		// };
-
-		// $scope.registerAccount = function(){
-        //     console.log('---registerAccount---');
-		// }
-		
-		// $scope.signIn = function(){
-        //     console.log('---signIn---');
-        //     animateLogin();
-		// };
 		
 		$scope.login = function () {
 			if($scope.username && $scope.password){
