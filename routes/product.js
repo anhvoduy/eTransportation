a16e32114650af2ac0d1f625b7a13aff9107735a
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-09-07 17:04:29 +0700

    handle server route

---
+++ routes/product.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+const Q = require('q');
+const constant = require('../lib/constant');
+const errorHelper = require('../lib/errorHelper');
+const validator = require('../lib/validator');
+const auth = require('../services/authService');
+const productService = require('../services/productService');
+
+router.get('/list', Q.async(function* (req, res, next) {
+	try
+	{
+		let products = yield productService.getList();
+		res.status(200).json(products);
+	}
+	catch(err){
+		res.status(500).json(err);
+		next(err);
+	}
+}));
+
+router.get('/item', Q.async(function* (req, res, next) {
+	try
+	{
+		let query = _.pick(req.query, ['ProductKey']);
+		let product = yield productService.getItem(query.ProductKey);
+		res.status(200).json(product);
+	}
+	catch(err){
+		res.status(500).json(err);
+		next(err);
+	}
+}));
+
+router.post('/create', auth.checkAuthentication(), function (req, res, next) {	
+	res.status(200).json(true);
+	next();
+});
+
+router.put('/update', auth.checkAuthentication(), function (req, res, next) {
+    res.status(200).json(true);
+	next();
+});
+
+router.delete('/delete', auth.checkAuthentication(), function (req, res, next) {	
+	res.status(200).json(true);
+	next();
+});
+
+module.exports = router;
---
+++ services/productService.js
@@ -0,0 +1,60 @@
+﻿const _ = require('lodash');
+const Q = require('q');
+const dbContext = require('../lib/dbContext');
+
+// Constructor
+const Factory = function(){	
+}
+
+Factory.prototype.getList = Q.async(function* (){
+    try
+    {
+        let sql = `
+            SELECT * 
+            FROM Product
+            WHERE Deleted = 0 
+            ORDER BY ProductId DESC
+        `;
+        yield dbContext.openConnection();
+        let products = yield dbContext.queryList(sql);
+        yield dbContext.closeConnection();
+        return products;
+    }
+    catch(err){
+        yield dbContext.closeConnection();
+        return err;
+    }
+})
+
+Factory.prototype.getItem = Q.async(function* (ProductKey){
+    try
+    {        
+        let sql = `
+            SELECT *
+            FROM Product
+            WHERE ProductKey = @ProductKey AND Deleted = 0
+        `;
+        yield dbContext.openConnection();
+        let product = yield dbContext.queryItem(sql, { ProductKey: ProductKey });
+        yield dbContext.closeConnection();
+        return product;
+    }catch(err){
+        yield dbContext.closeConnection();        
+        return err;
+    }
+});
+
+Factory.prototype.create = function(brand){
+    return true;
+}
+
+Factory.prototype.update = function(brand){
+    return true;
+}
+
+Factory.prototype.delete = function(BrandKey){
+    return true;
+}
+
+// Export
+module.exports = new Factory;
