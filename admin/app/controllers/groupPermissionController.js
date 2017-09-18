75636b1b2c23d6167e90b7cef976c963bcfe76e6
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-09-18 14:55:00 +0700

    seperate dialogController

--- admin/app/controllers/groupPermissionController.js
+++ admin/app/controllers/groupPermissionController.js
@@ -98,25 +98,3 @@
 		activate();
 	}
 })();
-
-
-(function () {
-	'use strict';
-	app.controller('dialogController', dialogController);
-	dialogController.$inject = ['$rootScope', '$scope', 'close'];
-	function dialogController($rootScope, $scope, close){
-		// models
-
-		// functions
-		var activate = function(){
-
-		}
-
-		$scope.close = function (result) {
-            close(result, 500); // close, but give 500ms for bootstrap to animate
-        };
-		
-		// start
-		activate();
-	}
-})();
